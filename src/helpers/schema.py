e71cee8cee8d13211cfb121c2530826034a6efde
Danii <daniilZakharchenko@gmail.com>
2021-05-24 14:35:32 +1000

    - set_value is public now

--- src/helpers/schema.py
+++ src/helpers/schema.py
@@ -16,7 +16,7 @@ class ApiSchema:
             if self._app.config['API_SCHEMA']['resources'][i]['rel'] == rel:
                 return self._app.config['API_SCHEMA']['resources'][i]['href']
 
-    def _set_value(self, rel: str, key: str, value: Any):
+    def set_value(self, rel: str, key: str, value: Any):
         for i in range(len(self._app.config['API_SCHEMA']['resources'])):
             if self._app.config['API_SCHEMA']['resources'][i]['rel'] == rel:
                 self._app.config['API_SCHEMA']['resources'][i][key] = value
@@ -24,5 +24,5 @@ class ApiSchema:
     def create_schema(self):
         self._app.config['API_SCHEMA'] = load(open('api_schema.json', 'r', encoding='utf-8'))
 
-        self._set_value('home', 'href', self._app.config.get('BASE_URL') or 'http://127.0.0.1:5000/')
-        self._set_value('base_uri', 'href', self._app.config.get('BASE_URL') or 'http://127.0.0.1:5000/')
+        self.set_value('home', 'href', self._app.config.get('BASE_URL') or 'http://127.0.0.1:5000/')
+        self.set_value('base_uri', 'href', self._app.config.get('BASE_URL') or 'http://127.0.0.1:5000/')
